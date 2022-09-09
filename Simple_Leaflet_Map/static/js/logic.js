d3233c51b41a5d8f5d291ca0e7e70a0660cdaa24
Laurina LaStella <106928784+laurinalastella@users.noreply.github.com>
2022-09-08 17:55:25 -0700

    Mod 13.4.1.

---
+++ Simple_Leaflet_Map/static/js/logic.js
@@ -0,0 +1,47 @@
+// Mod 13.2.4
+
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+// We're assigning the variable map to the object L.map(), and we'll instantiate the object with the given string 'mapid'.
+// The mapid will reference the id tag in our <div> element on the index.html file.
+// The setView() method sets the view of the map with a geographical center, where the first coordinate is latitude (40.7) and the second is longitude (-94.5). We set the zoom level of "4" on a scale 0–18.
+
+
+// We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    // attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
+
+// To change the map's style, change the map id using the list of Mapbox ids below:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
+
+// To use the Mapbox Styles API, edit the URL in the Leaflet tilelayer()method, as detailed on the Leaflet website:
+
+// First, navigate to the Mapbox Glossary (Links to an external site.).
+// Search the Static Tiles API (Links to an external site.).
+// Copy this part of the URL: https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/.
+// In your tileLayer() code, replace this part of the URL, https://api.tiles.mapbox.com/v4/{id}/, with the Mapbox Styles API URL you copied.
+// Remove the .png from the URL.
+// Remove the id attribute and the map style reference.
+// The code for the tileLayer() should look like the following:
+
+
+
