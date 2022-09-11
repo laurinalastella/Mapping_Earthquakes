c3793b116ea45bb70523d655c68910b13354f8fb
Laurina LaStella <106928784+laurinalastella@users.noreply.github.com>
2022-09-11 13:15:22 -0700

    Mod 13.6.6 manual upload.

--- static/js/logicStep3.js
+++ static/js/logicStep3.js
@@ -27,7 +27,7 @@ let map = L.map('mapid', {
   center: [39.5, -98.5],
   zoom: 3,
   layers: [streets]
-})
+});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
@@ -38,33 +38,58 @@ let quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week
 
 d3.json(quakes).then(function(data) {
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-
-  // We turn each feature into a circleMarker on the map.
-  
-  pointToLayer: function(feature, latlng) {
-              console.log(data);
-              return L.circleMarker(latlng);
-          },
-        // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo
-      }).addTo(map);
-  });
+    L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+    style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+    layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
+}).addTo(map);
+});
 
 // This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
+// the map. We pass the magnitude of the earthquake into two separate functions
+// to calculate the color and radius.
 function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
-  }
+  };
+
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  };
+
 
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
@@ -73,7 +98,7 @@ function getRadius(magnitude) {
     return 1;
   }
   return magnitude * 4;
-}
+};
 
 // To change the map's style, change the map id using the list of Mapbox ids below:
 // mapbox/streets-v11
@@ -93,6 +118,4 @@ function getRadius(magnitude) {
 // Remove the .png from the URL.
 // Remove the id attribute and the map style reference.
 // The code for the tileLayer() should look like the following:
-
-
 
--- static/js/logicStep4.js
+++ static/js/logicStep4.js
@@ -15,56 +15,99 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
   accessToken: API_KEY
 });
 
-
 // Create a base layer that holds both maps.
 let baseMaps = {
   "Streets": streets,
   "Satellite": satelliteStreets
 };
 
+// Create the earthquake layer for our map.
+let earthquakes = new L.layerGroup();
+
+// We define an object that contains the overlays.
+// This overlay will be visible all the time.
+let overlays = {
+    Earthquakes: earthquakes
+  };
+
+
+
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [39.5, -98.5],
-  zoom: 3,
-  layers: [streets]
-})
-
-// Pass our map layers into our layers control and add the layers control to the map.
-L.control.layers(baseMaps).addTo(map);
-
-
-// Retrieve the earthquake GeoJSON data.
-let quakes = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
-
-d3.json(quakes).then(function(data) {
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-
-  // We turn each feature into a circleMarker on the map.
-  
-  pointToLayer: function(feature, latlng) {
-              console.log(data);
-              return L.circleMarker(latlng);
-          },
-        // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo
-      }).addTo(map);
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
   });
 
+
+// Then we add a control to the map that will allow the user to change
+// which layers are visible.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+
+
+
+  
+
+// Retrieve the earthquake GeoJSON data.
+let quakesjson = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+d3.json(quakesjson).then(function(data) {
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+    // We turn each feature into a circleMarker on the map.
+    pointToLayer: function(feature, latlng) {
+        console.log(data);
+        return L.circleMarker(latlng);
+      },
+    // We set the style for each circleMarker using our styleInfo function.
+  style: styleInfo,
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+  }
+}).addTo(earthquakes);
+    // Add earthquake layer to map.
+    earthquakes.addTo(map)
+});
+
 // This function returns the style data for each of the earthquakes we plot on
-// the map. We pass the magnitude of the earthquake into a function
-// to calculate the radius.
+// the map. We pass the magnitude of the earthquake into two separate functions
+// to calculate the color and radius.
 function styleInfo(feature) {
     return {
       opacity: 1,
       fillOpacity: 1,
-      fillColor: "#ffae42",
+      fillColor: getColor(feature.properties.mag),
       color: "#000000",
-      radius: getRadius(),
+      radius: getRadius(feature.properties.mag),
       stroke: true,
       weight: 0.5
     };
-  }
+  };
+
+
+// This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude > 4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  };
+
 
 // This function determines the radius of the earthquake marker based on its magnitude.
 // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
@@ -73,7 +116,7 @@ function getRadius(magnitude) {
     return 1;
   }
   return magnitude * 4;
-}
+};
 
 // To change the map's style, change the map id using the list of Mapbox ids below:
 // mapbox/streets-v11
@@ -93,6 +136,4 @@ function getRadius(magnitude) {
 // Remove the .png from the URL.
 // Remove the id attribute and the map style reference.
 // The code for the tileLayer() should look like the following:
-
-
 
